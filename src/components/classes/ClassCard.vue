50778af268c277d3b7259e6f1dafa861ca12b189
Justin <jrhoades93@gmail.com>
2020-05-10 14:09:53 -0600

    Scaffolded the classes page

---
+++ src/components/classes/ClassCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="cls-base">
+    <img class="cls-icon" src="https://picsum.photos/200" alt="" />
+    <div class="cls-info">
+      <div>Name:</div>
+      <div>Primary Ability:</div>
+      <div>Hp:</div>
+      <div>Spellcaster:</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "classCard",
+  props: ["cls"],
+  setup() {
+    return {};
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/styles";
+
+.cls-base {
+  height: 10rem;
+  width: 40rem;
+  border-radius: 2.5rem;
+  background-color: $primary;
+  display: flex;
+  flex-flow: row;
+}
+
+.cls-icon {
+  height: 7.5rem;
+  width: 7.5rem;
+  border-radius: 50%;
+  background-color: $accent;
+  border: solid 0.1rem $accent;
+  text-align: center;
+  margin: 1rem 0 0 1rem;
+}
+
+.cls-info {
+  margin: 2rem 0 0 2.5rem;
+}
+</style>
--- src/views/Class.vue
+++ src/views/Class.vue
@@ -1,11 +1,69 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="container">
+    <h1 class="title has-text-light">Classes</h1>
+    <div class="classes-layout">
+      <ClassCard class="class-card" />
+      <ClassCard class="class-card" />
+      <ClassCard class="class-card" />
+      <ClassCard class="class-card" />
+      <ClassCard class="class-card" />
+      <ClassCard class="class-card" />
+      <ClassCard class="class-card" />
+      <ClassCard class="class-card" />
+    </div>
+    <b-button type="is-info" class="new-button" size="is-large">
+      <i class="ra ra-forging ra-fw ra-3x" />
+    </b-button>
+  </div>
 </template>
 
 <script lang="ts">
+import ClassCard from "@/components/classes/ClassCard.vue";
 export default {
-  name: "class"
+  name: "class",
+  components: {
+    ClassCard
+  }
 };
 </script>
 
-<style lang="scss"></style>
+<style scoped lang="scss">
+@import "../assets/styles";
+
+.class-card {
+  margin-top: 2rem;
+}
+
+.classes-layout {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+
+  overflow-y: auto;
+  margin-bottom: 1rem;
+}
+
+.new-button {
+  height: 100px;
+  width: 100px;
+  background-color: $accent;
+  margin-left: auto;
+  margin-right: 50px;
+  border-radius: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@media only screen and (max-width: 600px) {
+  .classes-layout {
+    height: 45rem;
+  }
+}
+
+@media only screen and (min-width: 600px) {
+  .classes-layout {
+    height: 50rem;
+  }
+}
+</style>
