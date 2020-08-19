6897397d96efdef1e08f57dbaff77fc381e115a1
Justin <jrhoades93@gmail.com>
2020-08-19 09:33:07 -0600

    Background view page built

--- src/components/backgrounds/BackgroundCard.vue
+++ src/components/backgrounds/BackgroundCard.vue
@@ -1,50 +1,61 @@
 <template>
-  <div class="background-base">
-    <div><span>Name:</span> {{ background.name }}</div>
-    <div>{{ background.description.substring(0, 100) }}...</div>
-    <div class="background-tags">
-      <span>Boosts: </span>
-      <b-tag
-        v-for="boost in background.boosts"
-        :key="boost"
-        class="is-info background-tag"
-        size="is-medium"
-      >
-        {{ boost }}
-      </b-tag>
+  <div>
+    <div @click="openModal()" class="background-base">
+      <div><span>Name:</span> {{ background.name }}</div>
+      <div>{{ background.description.substring(0, 100) }}...</div>
+      <div class="background-tags">
+        <span>Boosts: </span>
+        <b-tag
+          v-for="boost in background.boosts"
+          :key="boost"
+          class="is-info background-tag"
+          size="is-medium"
+        >
+          {{ boost }}
+        </b-tag>
+      </div>
+      <div class="background-tags">
+        <span>Skills: </span>
+        <b-tag
+          v-for="skill in background.skills"
+          :key="skill.name"
+          class="is-info background-tag"
+          size="is-medium"
+          aria-close-label="Close tag"
+        >
+          {{ skill.name }}
+        </b-tag>
+      </div>
+      <div class="background-tags">
+        <span>Feats: </span>
+        <b-tag
+          v-for="feat in background.feats"
+          :key="feat.name"
+          class="is-dark background-tag"
+          size="is-medium"
+          aria-close-label="Close tag"
+        >
+          {{ feat.name }}
+        </b-tag>
+      </div>
     </div>
-    <div class="background-tags">
-      <span>Skills: </span>
-      <b-tag
-        v-for="skill in background.skills"
-        :key="skill.name"
-        class="is-info background-tag"
-        size="is-medium"
-        aria-close-label="Close tag"
-      >
-        {{ skill.name }}
-      </b-tag>
-    </div>
-    <div class="background-tags">
-      <span>Feats: </span>
-      <b-tag
-        v-for="feat in background.feats"
-        :key="feat.name"
-        class="is-dark background-tag"
-        size="is-medium"
-        aria-close-label="Close tag"
-      >
-        {{ feat.name }}
-      </b-tag>
-    </div>
+    <!--  Modal  -->
+    <b-modal :active.sync="backgroundView">
+      <BackgroundView :background="background" />
+    </b-modal>
   </div>
 </template>
 
 <script lang="ts">
+import BackgroundView from "@/components/backgrounds/BackgroundView.vue";
 import { Background } from "@/shared/types/Backgrounds";
+import { ref, Ref } from "@vue/composition-api";
 
 export default {
   name: "BackgroundCard",
+  components: {
+    BackgroundView
+  },
   props: {
     background: {
       type: [Background],
@@ -52,7 +63,11 @@ export default {
     }
   },
   setup() {
-    return {};
+    const backgroundView: Ref<boolean> = ref(false);
+    function openModal() {
+      backgroundView.value = true;
+    }
+    return { backgroundView, openModal };
   }
 };
 </script>
@@ -62,7 +77,7 @@ export default {
 
 .background-base {
   height: 15rem;
-  width: 45rem;
+  width: 40rem;
   padding: 1rem;
   border-radius: 2.5rem;
   background-color: $primary;
--- src/components/backgrounds/BackgroundView.vue
+++ src/components/backgrounds/BackgroundView.vue
@@ -1,13 +1,83 @@
 <template>
-  <div>
-    <h1>View</h1>
+  <div class="background-base">
+    <div><span>Name:</span> {{ background.name }}</div>
+    <div>{{ background.description }}</div>
+    <div class="background-tags">
+      <span>Boosts: </span>
+      <b-tag
+        v-for="boost in background.boosts"
+        :key="boost"
+        class="is-info background-tag"
+        size="is-medium"
+      >
+        {{ boost }}
+      </b-tag>
+    </div>
+    <div class="background-tags">
+      <span>Skills: </span>
+      <b-tag
+        v-for="skill in background.skills"
+        :key="skill.name"
+        class="is-info background-tag"
+        size="is-medium"
+        aria-close-label="Close tag"
+      >
+        {{ skill.name }}
+      </b-tag>
+    </div>
+    <div class="background-tags">
+      <span>Feats: </span>
+      <b-tag
+        v-for="feat in background.feats"
+        :key="feat.name"
+        class="is-dark background-tag"
+        size="is-medium"
+        aria-close-label="Close tag"
+      >
+        {{ feat.name }}
+      </b-tag>
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Background } from "@/shared/types/Backgrounds";
+
 export default {
-  name: "BackgroundView"
+  name: "BackgroundView",
+  props: {
+    background: {
+      type: [Background],
+      required: true
+    }
+  },
+  setup() {
+    return {};
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+@import "../../assets/styles";
+
+.background-base {
+  display: flex;
+  flex-flow: column;
+  padding: 3rem;
+  border-radius: 2.5rem;
+  background-color: $primary;
+
+  div {
+    margin-bottom: 4rem;
+  }
+}
+
+.background-tags {
+  display: flex;
+  align-items: center;
+}
+
+.background-tag {
+  margin: 0.5rem;
+}
+</style>
