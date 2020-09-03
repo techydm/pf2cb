7ec8ba210a1359238edbee0870f72ffacfc347e2
Justin <jrhoades93@gmail.com>
2020-09-03 14:49:14 -0600

    Basic system setting worked

--- src/services/system.ts
+++ src/services/system.ts
@@ -1,25 +1,43 @@
-import { createDir, Dir, readDir } from "tauri/api/fs";
+import { createDir, Dir, readDir, readTextFile, writeFile } from "tauri/api/fs";
 
-// TODO: app directory check for config data
+class SystemConfig {
+  version: string = "";
+  appDir: string = "";
+}
 
-// TODO: app init function to handle the building of app directory
+// TODO: app init function to handle the building of app directories
+export async function appInit() {
+  // TODO: Check for config data
+  const configsJSON = await readTextFile("pf2cb/setting.json", {
+    dir: Dir.Config
+  }).catch(async err => {
+    // Build the setting object
+    const settings = new SystemConfig();
+    settings.version = "0.1";
+    settings.appDir = "/home/justin/pf2cb";
 
-export function appInit() {
-  createDir("save_files", { dir: Dir.App })
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.log(err);
+    // Build the app directory
+    await createDir(settings.appDir).catch(err => {
+      console.error(err.toString());
     });
 
-  readDir("save_files", { dir: Dir.App })
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.log(err);
+    // Build the config directory
+    await createDir("pf2cb", { dir: Dir.Config }).catch(err => {
+      console.error(err.toString());
     });
+
+    // Write current settings into the config directory
+    await writeFile(
+      { path: "pf2cb/setting.json", contents: JSON.stringify(settings) },
+      { dir: Dir.Config }
+    ).catch(err => {
+      console.error(err.toString());
+    });
+
+    return JSON.stringify(settings);
+  });
+
+  console.log(configsJSON);
 }
 // TODO: app migration function for when the app directory is moved
 // TODO: Generic save function that specifies the object being saved and the record itself
