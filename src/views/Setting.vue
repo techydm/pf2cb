fe6a51e8989a6fdd7a889be619ecd3420b0036f0
Justin <jrhoades93@gmail.com>
2020-09-10 21:37:16 -0600

    Base setting page need to work

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="container">
-    <h1 class="title has-text-light">Settings</h1>
-    <SaveFolder />
+  <div class="container setting-page">
     <div>
-      <b-button @click="appInit()">Click me</b-button>
-    </div>
-    <div>
-      <b-button @click="saveFile(0, { name: 'Justin', age: 26 })"
-        >Save</b-button
-      >
+      <div class="title has-text-light">Setting</div>
+      <div>Directory: {{ appDir }}</div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import SaveFolder from "@/components/settings/SaveFolder.vue";
-import { appInit, saveFile } from "@/services/system";
+import { getAppDir } from "@/services/system";
 
 export default {
   name: "setting",
-  components: { SaveFolder },
   setup() {
-    return { appInit, saveFile };
+    const appDir = getAppDir();
+    return { appDir };
   }
 };
 </script>
+
+<style lang="scss">
+.setting-page {
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+}
+</style>
